d13b874ae4b272f63c249126fa9f6f40e65d7445
yanjie666 <936812250@qq.com>
2018-09-28 11:59:21 +0800

    brandService实现代码相关功能编写

--- pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
+++ pinyougou-sellergoods-service/src/main/java/com/pinyougou/sellergoods/service/impl/BrandServiceImpl.java
@@ -2,6 +2,10 @@ package com.pinyougou.sellergoods.service.impl;
 
 import java.util.List;
 
+import com.github.pagehelper.Page;
+import com.github.pagehelper.PageHelper;
+import com.pinyougou.pojo.TbBrandExample;
+import entity.PageResult;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.alibaba.dubbo.config.annotation.Service;
@@ -19,5 +23,53 @@ public class BrandServiceImpl implements BrandService {
         // TODO Auto-generated method stub
         return brandMapper.selectByExample(null);
     }
+
+    @Override
+    public PageResult findPage(int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        Page<TbBrand> page=   (Page<TbBrand>) brandMapper.selectByExample(null);
+        return new PageResult(page.getTotal(), page.getResult());
+
+    }
+
+    @Override
+    public void add(TbBrand brand) {
+        brandMapper.insert(brand);
+    }
+
+    @Override
+    public TbBrand findOne(Long id) {
+        return brandMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public void update(TbBrand brand) {
+        brandMapper.updateByPrimaryKey(brand);
+    }
+
+    @Override
+    public void delete(Long[] ids) {
+        for(Long id:ids){
+            brandMapper.deleteByPrimaryKey(id);
+        }
+    }
+
+    @Override
+    public PageResult findPage(TbBrand brand, int pageNum, int pageSize) {
+        PageHelper.startPage(pageNum, pageSize);
+        TbBrandExample example=new TbBrandExample();
+        TbBrandExample.Criteria criteria = example.createCriteria();
+        if(brand!=null){
+            if(brand.getName()!=null && brand.getName().length()>0){
+                criteria.andNameLike("%"+brand.getName()+"%");
+            }
+            if(brand.getFirstChar()!=null && brand.getFirstChar().length()>0){
+                criteria.andFirstCharEqualTo(brand.getFirstChar());
+            }
+        }
+        Page<TbBrand> page= (Page<TbBrand>)brandMapper.selectByExample(example);
+        return new PageResult(page.getTotal(), page.getResult());
+    }
+
 
 }
