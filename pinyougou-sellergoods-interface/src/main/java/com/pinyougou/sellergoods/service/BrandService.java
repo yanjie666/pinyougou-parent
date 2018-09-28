4f632e5729d3f42bcce0b4220ae1c2900337436a
yanjie666 <936812250@qq.com>
2018-09-28 11:58:33 +0800

    brandService代码相关功能编写

--- pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -1,9 +1,25 @@
 package com.pinyougou.sellergoods.service;
 
 import com.pinyougou.pojo.TbBrand;
+import entity.PageResult;
 
 import java.util.List;
 
 public interface BrandService {
     public List<TbBrand> findAll();
+
+    public PageResult findPage(int pageNum,int pageSize);
+
+
+    public void add(TbBrand brand);
+
+
+    public TbBrand findOne(Long id);
+
+    public void update(TbBrand brand);
+
+    public void delete(Long[] ids);
+
+
+    public PageResult findPage(TbBrand brand,int pageNum,int pageSize);
 }
