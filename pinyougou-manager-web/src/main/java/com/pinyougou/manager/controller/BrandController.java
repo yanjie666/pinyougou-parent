de3a163ba2d6d6147244c7c82fdef866ba01d907
yanjie666 <936812250@qq.com>
2018-09-28 12:02:59 +0800

    控制层代码相关功能编写

--- pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
+++ pinyougou-manager-web/src/main/java/com/pinyougou/manager/controller/BrandController.java
@@ -2,6 +2,9 @@ package com.pinyougou.manager.controller;
 
 import java.util.List;
 
+import entity.PageResult;
+import entity.Result;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +22,59 @@ public class BrandController {
     public List<TbBrand> findAll(){
         return brandService.findAll();
     }
+
+
+    @RequestMapping("/findPage")
+    public PageResult findPage(int page,int rows){
+        return brandService.findPage(page,rows);
+    }
+
+    @RequestMapping("/add")
+    public Result add(@RequestBody TbBrand brand){
+
+        try {
+            brandService.add(brand);
+            return new Result(true,"新增成功");
+        }catch (Exception e){
+            return new Result(false,"新增失败");
+        }
+
+    }
+
+
+    @RequestMapping("/findOne")
+    public TbBrand findOne(Long id){
+        return brandService.findOne(id);
+    }
+
+
+    @RequestMapping("/update")
+    public Result update(@RequestBody TbBrand brand){
+        try {
+            brandService.update(brand);
+            return new Result(true, "修改成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "修改失败");
+        }
+    }
+
+
+    @RequestMapping("/delete")
+    public Result delete(Long[] ids){
+        try {
+            brandService.delete(ids);
+            return new Result(true, "删除成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "删除失败");
+        }
+    }
+
+
+    @RequestMapping("/search")
+    public PageResult search(@RequestBody TbBrand brand,int page,int rows){
+        return brandService.findPage(brand,page,rows);
+    }
+
 }
