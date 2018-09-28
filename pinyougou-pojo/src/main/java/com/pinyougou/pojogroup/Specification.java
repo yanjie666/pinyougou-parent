82dd03ab9527f9e4cdbca55bba18801184d69136
yanjie666 <936812250@qq.com>
2018-09-28 23:31:54 +0800

    规格与规格选项实体类封装

---
+++ pinyougou-pojo/src/main/java/com/pinyougou/pojogroup/Specification.java
@@ -0,0 +1,47 @@
+package com.pinyougou.pojogroup;
+
+import com.pinyougou.pojo.TbSpecification;
+import com.pinyougou.pojo.TbSpecificationOption;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Specification implements Serializable {
+
+    private TbSpecification specification;
+    private List<TbSpecificationOption> specificationOptionList;
+
+    @Override
+    public String toString() {
+        return "Specification{" +
+                "specification=" + specification +
+                ", specificationOptionList=" + specificationOptionList +
+                '}';
+    }
+
+    public TbSpecification getSpecification() {
+        return specification;
+    }
+
+    public void setSpecification(TbSpecification specification) {
+        this.specification = specification;
+    }
+
+    public List<TbSpecificationOption> getSpecificationOptionList() {
+        return specificationOptionList;
+    }
+
+    public void setSpecificationOptionList(List<TbSpecificationOption> specificationOptionList) {
+        this.specificationOptionList = specificationOptionList;
+    }
+
+    public Specification(TbSpecification specification, List<TbSpecificationOption> specificationOptionList) {
+
+        this.specification = specification;
+        this.specificationOptionList = specificationOptionList;
+    }
+
+    public Specification() {
+
+    }
+}
